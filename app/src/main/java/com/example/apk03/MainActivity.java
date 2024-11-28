209b75cd01d2fbfe77073e87968b005eb8347709
Kauan <kauanribeirofsa@gmail.com>
2024-11-28 11:59:21 -0300

    Alterei as três telas

--- app/src/main/java/com/example/apk03/MainActivity.java
+++ app/src/main/java/com/example/apk03/MainActivity.java
@@ -4,8 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -29,6 +31,39 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent= new Intent(MainActivity.this, Tela2.class);
                 startActivity(intent);
+            }
+        });
+        Button button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Tela3.class);
+                startActivity(intent);
+            }
+        });
+        Button anonimo = findViewById(R.id.anonimo);
+        anonimo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Tela3.class);
+                startActivity(intent);
+            }
+        });
+        TextView Privacy =findViewById(R.id.Privacy);
+        Privacy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder buider = new AlertDialog.Builder(MainActivity.this);
+                buider.setTitle("Política de Privacidade");
+                buider.setMessage("Ao usar este aplicativo, você concorda com os nossos termos de uso e política de privacidade. Para mais informações, visite nosso site.");
+
+                buider.setPositiveButton("Aceitar", (dialog, which)->{
+
+                });
+                buider.setNegativeButton("Recusar", (dialog, which) -> {
+                    dialog.dismiss();
+                });
+                buider.show();
             }
         });
     }
--- app/src/main/java/com/example/apk03/Tela2.java
+++ app/src/main/java/com/example/apk03/Tela2.java
@@ -2,9 +2,12 @@ package com.example.apk03;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -23,6 +26,22 @@ public class Tela2 extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        Button FinishCadastro = findViewById(R.id.FinishCadastro);
+        FinishCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder buider = new AlertDialog.Builder(Tela2.this);
+                buider.setTitle("Cadastro");
+                buider.setMessage("Cadastro Finalizado com Sucesso!");
+                buider.setPositiveButton("ok", (dialog, which)->{
+
+                });
+                buider.setNegativeButton("Cancelar", (dialog, which)-> {
+                    dialog.dismiss();
+                });
+                buider.show();
+            }
         });
     }
 }
