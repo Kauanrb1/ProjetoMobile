4b08cdd02d1b136b5eefee3465b86fb8178ea9ce
Kauanrb1 <kauanribeirofsa@gmail.com>
2024-12-17 12:03:17 -0300

    alterei uns botões da tela principal e comecei a fazer a integração de login com google.

--- app/src/main/java/com/example/apk03/telaPricipal.java
+++ app/src/main/java/com/example/apk03/telaPricipal.java
@@ -1,6 +1,9 @@
 package com.example.apk03;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class telaPricipal extends AppCompatActivity {
-
+private boolean corDiferente = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +22,21 @@ public class telaPricipal extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        Button Geral = findViewById(R.id.Geral);
+
+        Drawable transparenteborda = getResources().getDrawable(R.drawable.button_transparente_borda);
+        Drawable coralterada = getResources().getDrawable(R.drawable.cor_alterada);
+        Geral.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(corDiferente){
+                    Geral.setBackground(transparenteborda);
+                } else {
+                    Geral.setBackground(coralterada);
+                }
+                corDiferente = !corDiferente;
+            }
         });
     }
 }
